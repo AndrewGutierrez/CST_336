4af492ba95aa40ad75587e62b851c3fbd862e59f
Andrew Gutierrez <andgutierrez@csumb.edu>
2017-09-12 19:46:14 +0000

    Lab 2

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
         <a href="lab1/index.html">Lab 1</a>
         <br></br>
         <a href="homework1/index.html">Homework 1</a>
+        <br></br>
+        <a href="lab2/777/index.php">Lab 2</a>
     </body>
 </html>
